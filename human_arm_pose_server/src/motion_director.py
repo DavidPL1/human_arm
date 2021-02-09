f425f5ba38939eb15b6afca015bd2e8a23c2d579
dleins <dleins@techfak.uni-bielefeld.de>
2021-02-09 18:21:27 +0100

    Adds motion server and motion director

---
+++ human_arm_pose_server/src/motion_director.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+
+import rospy
+from time import sleep
+
+from actionlib import SimpleActionClient
+from human_arm_motion_msgs.msg import PlayGestureAction, PlayGestureGoal
+from visualization_msgs.msg import Marker
+from std_msgs.msg import UInt8, Empty
+
+class MotionDirector():
+    def __init__(self):
+        rospy.init_node('human_arm_motion_director')
+        self.text_frame_id = rospy.get_param('~context_info_frame_id', 'forearm_base')
+        self.rec_steps = rospy.get_param('~rec_steps')
+        self.pub_marker = rospy.Publisher('visualization_marker', Marker, queue_size=1)
+        self.pub_rec_flag = rospy.Publisher('rec_flag', UInt8, queue_size=1)
+        self.current_timer = None
+        self.pub_rec_flag.publish(UInt8(0))
+
+        rospy.loginfo("Connecting to motion server")
+        self.motion_server = SimpleActionClient('play_gesture', PlayGestureAction)
+        rospy.loginfo('Waiting for motion server')
+        server_up = self.motion_server.wait_for_server(rospy.Duration(10.0))
+        if not server_up:
+            rospy.logerr('Timed out waiting for motion server')
+            rospy.signal_shutdown('Timed out waiting for motion server')
+
+        rospy.loginfo("Setup done, starting to play rec motion config")
+
+        rospy.Subscriber('play_config', Empty, self.parse_config)
+        self.pub_marker_text('Motion director waiting to start')
+
+
+    def start_recording(self):
+        self.pub_marker_text("Recording", is_red=True)
+        self.pub_rec_flag.publish(UInt8(1))
+
+
+    def end_recording(self):
+        self.pub_marker_text("")
+        self.pub_rec_flag.publish(UInt8(0))
+
+
+    def pub_marker_text(self, text, is_red=False):
+        marker = Marker(
+            type = Marker.TEXT_VIEW_FACING,
+            text = text,
+            id = 0,
+            ns = "context_info"
+        )
+
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        if not is_red:
+            marker.color.g, marker.color.b = 1.0, 1.0
+
+        marker.scale.x, marker.scale.y, marker.scale.z = 0.1, 0.1, 0.1
+        marker.pose.position.z = 0.55
+        marker.pose.orientation.w = 1.0
+        marker.header.frame_id = self.text_frame_id
+
+        self.pub_marker.publish(marker)
+
+
+    def feedback_cb(self, feedback):
+        if self.current_timer and self.current_timer >= feedback.seconds_left:
+            self.start_recording()
+
+
+    def done_cb(self, state, result):
+        self.end_recording()
+
+
+    def parse_config(self, empty):
+
+        rospy.loginfo("Parsing and executing steps")
+
+        rospy.logerr('rec_steps: /n%s' % self.rec_steps)
+
+        for step_name in self.rec_steps:
+            rospy.loginfo('Processing step %s' % step_name)
+            step = self.rec_steps.get(step_name)
+
+            fam_reps = int(step['familiarization_repeats'])
+            repeats = int(step['repeats']) + fam_reps
+            rep_period = float(step['period_seconds'])
+
+            rospy.logerr("fam_reps: %s, reps: %s, rep_period: %s, gesture: %s" % (fam_reps, repeats, rep_period, step['gesture_name']))
+
+            self.current_timer = repeats * rep_period - fam_reps * rep_period
+
+            if 'wait_seconds' in step.keys():
+                secs = int(step.get('wait_seconds'))
+                while secs > 0:
+                    self.pub_marker_text('Waiting... %ds' % secs)
+                    secs -= 1
+                    sleep(1)
+
+            self.pub_marker_text('Familiarization Phase')
+
+            motion_goal = PlayGestureGoal(
+                repeats=repeats,
+                period_seconds=rep_period,
+                gesture_name=step['gesture_name']
+            )
+
+            if 'start_gesture_name' in step.keys():
+                motion_goal.start_gesture_name = step.get('start_gesture_name')
+
+            self.motion_server.send_goal(
+                motion_goal,
+                feedback_cb=self.feedback_cb,
+                done_cb=self.done_cb,
+            )
+
+            if self.motion_server.wait_for_result(rospy.Duration.from_sec(repeats * rep_period + 15)):
+                rospy.loginfo('Done with %s' % step_name)
+            else:
+                rospy.logerr('Timed out on %s' % step_name)
+
+        self.pub_marker_text('Motion director waiting to start')
+
+if __name__ == "__main__":
+    director = MotionDirector()
+    rospy.spin()
---
+++ human_arm_pose_server/src/motion_server.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import rospy
+import numpy as np
+import yaml
+
+from time import sleep
+
+import actionlib
+
+from sensor_msgs.msg import JointState
+from human_arm_motion_msgs.srv import SaveGesture, SaveGestureResponse
+from human_arm_motion_msgs.msg import PlayGestureAction, PlayGestureGoal, PlayGestureFeedback, PlayGestureResult
+
+from rosgraph_msgs.msg import Clock
+
+class MotionServer():
+    def __init__(self):
+        rospy.init_node('human_arm_motion_server')
+
+        shadow_preview = rospy.get_param('~shadow_preview', True)
+        self.sample_frequency = rospy.get_param('~sample_frequency', 20)
+        self.target_prefix = target_prefix = rospy.get_param('~target_prefix', 'goal')
+        self.start_prefix = start_prefix = rospy.get_param('~start_prefix', 'start')
+        self.gestures = rospy.get_param('gestures/')
+        self.pub_start = None
+        self.pub_target = None
+        self.sim_time = rospy.get_param('use_sim_time', False)
+
+        self.sim_time = rospy.get_param('use_sim_time', False)
+        self.clock_pub = rospy.Publisher('clock', Clock, queue_size=1)
+        self.clock_msg = Clock()
+
+        self.pub_model = rospy.Publisher('input/joint_states', JointState, queue_size=1)
+        if shadow_preview:
+            self.pub_start = rospy.Publisher('%s/input/joint_states' % start_prefix, JointState, queue_size=1)
+            self.pub_target = rospy.Publisher('%s/input/joint_states' % target_prefix, JointState, queue_size=1)
+
+        self.motion_as = actionlib.SimpleActionServer('play_gesture', PlayGestureAction,
+                                                      execute_cb=self.play_gesture, auto_start=False)
+        self.save_srv = rospy.Service('save_gesture', SaveGesture, self.handle_gesture_save)
+
+        self.motion_as.start()
+        rospy.loginfo("Pose server init complete, ready to save and play gestures")
+        rospy.spin()
+
+
+    def handle_gesture_save(self, req):
+        mode = 0  # all
+
+        if len(req.joint_whitelist) > 0:
+            mode = 1  # whitelist
+        if len(req.joint_blacklist) > 0 and req.joint_blacklist[0] != '':
+            if mode == 1:
+                rospy.logerr("joint whitelist and blacklist can't be supplied simoultaneously!")
+                return SaveGestureResponse(SaveGestureResponse.ARGUMENT_ERROR)
+            mode = 2  # balcklist
+
+        current_pose_msg = rospy.wait_for_message('joint_states', JointState)
+
+        state_dict = dict(
+            joint=current_pose_msg.name,
+            position=current_pose_msg.position
+        )
+
+        if mode == 1:
+            curr_pose_dict = {name:pos for name,pos in zip(current_pose_msg.name, current_pose_msg.position) if name in req.joint_whitelist}
+            state_dict = dict(
+                joint=curr_pose_dict.keys(),
+                position=curr_pose_dict.values()
+            )
+        if mode == 2:
+            curr_pose_dict = {name:pos for name,pos in zip(current_pose_msg.name, current_pose_msg.position) if name not in req.joint_blacklist}
+            state_dict = dict(
+                joint=curr_pose_dict.keys(),
+                position=curr_pose_dict.values()
+            )
+
+        gesture_dict = {}
+        gesture_dict[req.gesture_name] = state_dict
+
+        # upload to param server and optionally save as yaml, if filename is provided
+        self.gestures[req.gesture_name] = state_dict
+        rospy.set_param('gestures', self.gestures)
+
+        if req.file_name is not None:
+            if not os.path.exists(os.path.dirname(req.file_name)) and os.path.dirname(req.file_name) != '':
+                return SaveGestureResponse(SaveGestureResponse.INVALID_PATH)
+            with open(req.file_name, 'w') as f:
+                yaml.dump(gesture_dict, f)
+                rospy.loginfo('Saved gesture in: %s' % os.path.abspath(req.file_name))
+
+        return SaveGestureResponse(SaveGestureResponse.SUCCESS)
+
+    def play_gesture(self, goal):
+        _result = PlayGestureResult()
+        _feedback = PlayGestureFeedback(seconds_left=-1, repeat_counter=0)
+
+        if goal.gesture_name not in self.gestures:
+            _result.return_state = _result.TARGET_GESTURE_UNKNOWN
+            self.motion_as.set_succeeded(_result)
+            return
+
+        current_pose_msg = rospy.wait_for_message('joint_states', JointState)
+        start_pose_dict = {name:pos for name,pos in zip(current_pose_msg.name, current_pose_msg.position)}
+
+        # If not provided, use current pose as start
+        if goal.start_gesture_name in self.gestures:
+            rospy.logdebug('starting with gesture "%s"' % goal.start_gesture_name)
+            start_gst = self.gestures.get(goal.start_gesture_name)
+            debug_joint_name = start_gst.get('joint')[0]
+
+            start_gst_dict = {name:pos for name,pos in zip(start_gst.get('joint'), start_gst.get('position'))}
+            # Override only the joints defined by the start gesture
+            start_pose_dict = {name:start_gst_dict.get(name, start_pose_dict[name]) for name in start_pose_dict}
+
+        stamp = rospy.Time.from_sec(t) if self.sim_time else rospy.Time.now()
+        self.clock_msg.clock = stamp
+        self.clock_pub.publish(self.clock_msg)
+
+        msg_syn = JointState()
+        msg_syn.header.stamp = stamp
+        msg_syn.name = list(start_pose_dict.keys())
+        msg_syn.position = list(start_pose_dict.values())
+        self.pub_start.publish(msg_syn)
+        self.pub_model.publish(msg_syn)
+
+        target_gst = self.gestures.get(goal.gesture_name)
+        target_gst_dict = {name:pos for name,pos in zip(target_gst.get('joint'), target_gst.get('position'))}
+        target_pose_dict = {name:target_gst_dict.get(name, start_pose_dict[name]) for name in start_pose_dict}
+
+        msg_syn.name = list(target_pose_dict.keys())
+        msg_syn.position = list(target_pose_dict.values())
+        self.pub_target.publish(msg_syn)
+
+        rate = rospy.Rate(self.sample_frequency) # running at 10 Hz
+        t = 0 # counter for elapsed time
+        period = goal.period_seconds  # includes movement to target and back to start
+        repetitions = goal.repeats
+        end = period * repetitions
+
+        _feedback.seconds_left = end
+
+        # dict keys() and value() functions return the list in their own order so this is necessary to ensure
+        # the different values of the same joints are being subtracted
+        diff = [start_pose_dict[name] - target_pose_dict[name] for name in target_pose_dict.keys()]
+
+        self.motion_as.publish_feedback(_feedback)
+
+        while t < end:
+            stamp = rospy.Time.from_sec(t) if self.sim_time else rospy.Time.now()
+            self.clock_msg.clock = stamp
+            self.clock_pub.publish(self.clock_msg)
+
+            msg_syn.header.stamp = stamp
+
+            for i, pos in enumerate(target_pose_dict.values()):
+                msg_syn.position[i] = pos + diff[i] * (0.5 + 0.5 * np.cos(2.*np.pi/period*t))
+                self.pub_model.publish(msg_syn)
+            t += rate.sleep_dur.to_sec()
+            sleep(rate.sleep_dur.to_sec())
+            _feedback.seconds_left = end - t
+            _feedback.repeat_counter = t // period
+            self.motion_as.publish_feedback(_feedback)
+        rospy.logdebug("Took %f seconds for %d repetitions of %d seconds" % (t, repetitions, period))
+
+        self.pub_target.publish(msg_syn)
+        self.pub_start.publish(msg_syn)
+        _result.return_state  = _result.SUCCESS
+        self.motion_as.set_succeeded(_result)
+
+if __name__ == "__main__":
+    motion_server = MotionServer()
